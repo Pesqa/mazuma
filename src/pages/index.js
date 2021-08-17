6493c82741d071e36a5c9d599439576f27ef0f50
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-08-17 20:41:01 +0530

    homepage design implementation starting

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,8 +41,7 @@ const IndexPage = () => {
               <a onClick={openModal} className="text-bg-info fs-6 text-decoration-none">
               <svg viewBox="0 0 1200 1200"><path d="M 600,1200 C 268.65,1200 0,931.35 0,600 0,268.65 268.65,0 600,0 c 331.35,0 600,268.65 600,600 0,331.35 -268.65,600 -600,600 z M 450,300.45 450,899.55 900,600 450,300.45 z" id="path16995" /></svg>See the Lemonade App in action</a>
             </div>
-          </div>
-          <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="RvRMFXela7E" onClose={() => setOpen(false)} />
+          </div>          
           {/* <div className="col-xl-5 col-lg-7 col-md-9"> <img src="images/mob_app.png" alt=""/> </div> */}
         </div>
       </div>
