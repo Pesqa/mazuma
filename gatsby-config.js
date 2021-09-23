4ded0967d8ab21fbccbfc02c7222b261225cf774
nanirama <nani.rama1982@gmail.com>
2021-09-23 08:28:56 +0530

    mailchimp integration 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Mazuma Accountants`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteURL: `https://www.mazumamoney.co.uk`,
